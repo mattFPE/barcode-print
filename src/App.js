15796080cfe4959b773f7fa73ea665c93cc39669
Matt Carter <matt.carter@fleeceperformance.com>
2022-10-05 13:55:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react';
+// import Barcode from 'react-barcode'
+
+// function App() {
+//   const [barcodeValue, setBarcodeValue] = useState('')
+
+//   const handleText = (e) => setBarcodeValue(e.target.value)
+//   return (
+//     <div>
+//       <input type='text' value={barcodeValue} onChange={handleText} />
+//       {barcodeValue !== '' && <Barcode value={barcodeValue} />}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useRef, useState } from 'react';
+import Barcode from 'react-barcode'
+import { useReactToPrint } from 'react-to-print'
 
 function App() {
+  const componentRef = useRef()
+  const handlePrint = useReactToPrint({ content: () => componentRef.current })
+  const [barcodeValue, setBarcodeValue] = useState('')
+
+  const handleText = (e) => setBarcodeValue(e.target.value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {barcodeValue !== '' && <Barcode value={barcodeValue} ref={componentRef} />}
+      <input type='text' value={barcodeValue} onChange={handleText} />
+      <button onClick={handlePrint}>Print this out!</button>
     </div>
   );
 }
